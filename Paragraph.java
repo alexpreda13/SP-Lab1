5113ab134c87943b750800aae177243ad08081e1
alexpreda13 <alexandru.preda99@e-uvt.ro>
2020-11-15 11:16:56 +0200

    lab3

---
+++ Paragraph.java
@@ -0,0 +1,35 @@
+package MainPackage;
+
+public class Paragraph implements Element {
+    String pText;
+    AlignStrategy alStr;
+    Paragraph(String text) {
+        this.pText = text;
+    }
+
+
+
+    public void setAlignStrategy(AlignStrategy alStr) {
+        this.alStr = alStr;
+    }
+
+    public String getpText() {
+        return pText;
+    }
+    @Override
+    public String toString() {
+        return "Paragraph{" +
+                "pText='" + pText + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public void print() {
+        if(alStr == null) {
+            System.out.println(this.toString());
+            return;
+        }
+        alStr.render(this, new Context());
+    }
+}
